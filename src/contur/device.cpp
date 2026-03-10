c4b20c224a8957da9236e9f15eaa69a971215904
tokKurumi <tkurumi901@gmail.com>
2026-03-10 15:35:01 +0300

    remove legacy

--- src/contur/device.cpp
+++
@@ -1,7 +0,0 @@
-#include "contur/device.h"
-#include <iostream>
-
-void Device::printData(int data)
-{
-    std::cout << "Device printData: data = " << data << '\n';
-}
--- src/contur/lan.cpp
+++
@@ -1,7 +0,0 @@
-#include "contur/lan.h"
-#include <iostream>
-
-void LAN::sendData(int data)
-{
-    std::cout << "LAN send: data = " << data << '\n';
-}
